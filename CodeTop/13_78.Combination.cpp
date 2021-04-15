d25a85a0333c2f384510e9c67d2362952d6e30b5
Lei Yu <Lei.Yu@MediaKind.com>
2021-04-15 18:31:02 +0800

    edit

---
+++ CodeTop/13_78.Combination.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=cpp
+ *
+ * [78] 子集 全组合
+ */
+
+// @lc code=start
+
+
+
+
+
+//直接从前遍历，遇到一个数就把所有子集加上该数组成新的子集，遍历完毕即是所有子集
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> tmp;
+        res.push_back(tmp);
+        for(int i=0;i<nums.size();i++){
+            int size=res.size();
+            for(int j=0;j<size;j++){
+                tmp=res[j];
+                tmp.push_back(nums[i]);
+                res.push_back(tmp);
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
