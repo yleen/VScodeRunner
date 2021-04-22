615b0f3427f93f361b7f024e12ceec3848a60a39
Lei Yu <Lei.Yu@MediaKind.com>
2021-04-22 16:15:48 +0800

    :pencil:day ex

--- Golang/helloword.go
+++ Golang/helloword.go
@@ -1,18 +1,25 @@
 package main //声明了 main.go 所在的包，Go 语言中使用包来组织代码。一般一个文件夹即一个包，包内可以暴露类型或方法供其他包使用。
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //fmt 是 Go 语言的一个标准库/包，用来处理标准输入输出。
 
-func main() { //main 函数是整个程序的入口，main 函数所在的包名也必须为 main。
-	age := 18
-	if age > 14 {
-		fmt.Println("yes")
-	} else {
-		fmt.Println("warning")
+func main() {
+	for i := 0; i < 3; i++ {
+		go download("www.abc.com")
 	}
-	if age := 18; age > 14 {
-		fmt.Println("warning")
-	} else {
-		fmt.Println("yes")
+	for i := 0; i < 3; i++ {
+		msg := <-ch
+		fmt.Println("Finish:", msg)
 	}
+	fmt.Println("Done!")
+}
+
+var ch = make(chan string, 10) //创建大小为10的缓冲信道
+func download(url string) {
+	fmt.Println("start to download", url)
+	time.Sleep(time.Second)
+	ch <- url
 }
---
+++ gitpush.bat
@@ -0,0 +1,7 @@
+@echo off
+F：
+cd F:\PrCode\C
+git config --global credential.helper store
+git add .
+git commit -m ":pencil:day ex"
+git push
