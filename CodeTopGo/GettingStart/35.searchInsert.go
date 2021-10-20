2ea2ae16f3e8380cc3147eb3115212e17c791e26
Allen <yuleen_@outlook.com>
2021-10-20 20:56:52 +0800

    rename codetopgo

---
+++ CodeTopGo/GettingStart/35.searchInsert.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=golang
+ *
+ * [35] 搜索插入位置
+ * [easy]
+ * https://leetcode-cn.com/problems/search-insert-position/
+ */
+
+// @lc code=start
+package gettingstart
+
+func searchInsert(nums []int, target int) int {
+	return 0
+}
+
+// @lc code=end
---
+++ CodeTopGo/GettingStart/977.sortedSqares.go
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=golang
+ *
+ * [977] 有序数组的平方
+ * [easy]
+ * https://leetcode-cn.com/problems/squares-of-a-sorted-array/
+ */
+
+// @lc code=start
+package gettingstart
+
+func sortedSquares(nums []int) []int {
+	return nums
+}
+
+//三种方法
+// @lc code=end
