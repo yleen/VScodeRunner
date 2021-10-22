61f75dcc8d7d34ad49ddfdcdefbd22306166f417
yleen <yuleen_@outlook.com>
2021-10-22 17:17:14 +0800

    15

---
+++ CodeTopGo/15.threeSum.go
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=golang
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+func threeSum(nums []int) [][]int {
+	sumList [][]int
+	sort.Int(nums)
+	length := len(nums)
+	for i := 0; i < 
+}
+// @lc code=end
+
