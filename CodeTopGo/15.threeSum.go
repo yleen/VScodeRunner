4f76d2830764af465a4ff62f17f702f2d6433900
yleen <yuleen_@outlook.com>
2021-10-26 19:20:38 +0800

    go lc

--- CodeTopGo/15.threeSum.go
+++ CodeTopGo/15.threeSum.go
@@ -6,10 +6,36 @@
 
 // @lc code=start
 func threeSum(nums []int) [][]int {
-	sumList [][]int
-	sort.Int(nums)
+	sumList := make([][]int,0)
+	sort.Ints(nums)
 	length := len(nums)
-	for i := 0; i < 
+	for i := 0; i < length; i++{
+		first := i
+		if nums[first] > 0 {
+			break
+		}
+		if first > 0 && nums[first] == nums[first - 1] {
+			continue
+		}
+		target := -nums[first]
+		third := length - 1
+		for second := first + 1; second < length; second++{
+			if second > first + 1 && nums[second] == nums[second - 1]{
+				continue
+			}
+			for second < third && nums[second] + nums[third] > target{
+				third--
+			}
+			if second == third{
+				break
+			}
+			if nums[second] + nums[third] == target{
+				sumList = append(sumList,[]int{nums[first],nums[second],nums[third]})
+			}
+		}
+	}
+
+	return sumList
 }
 // @lc code=end
 
---
+++ CodeTopGo/53.maxSubArray.go
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=golang
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+func maxSubArray(nums []int) int {
+	maxSub := 0
+	currSub := 0
+	for i,v := range nums{
+		currSub =
+	}
+}
+
+// @lc code=end
+
