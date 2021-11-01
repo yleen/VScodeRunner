b988b8012a900f5add2255587ac94ef6a669578b
yleen <yuleen_@outlook.com>
2021-11-01 09:29:57 +0800

    53

--- CodeTopGo/53.maxSubArray.go
+++ CodeTopGo/53.maxSubArray.go
@@ -6,10 +6,20 @@
 
 // @lc code=start
 func maxSubArray(nums []int) int {
-	maxSub := 0
+	maxSub := nums[0]
 	currSub := 0
-	for i,v := range nums{
-		currSub =
+	for _,v := range nums{
+		currSub = maxNum(v + currSub,v)
+		maxSub = maxNum(currSub, maxSub)
+	}
+	return maxSub
+}
+
+func maxNum(a int, b int) int{
+	if a > b{
+		return a
+	}else{
+		return b
 	}
 }
 
