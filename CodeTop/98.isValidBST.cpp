439519c8f40716c4aee08f10612774743a708796
yleen <yuleen_@outlook.com>
2021-12-02 18:46:34 +0800

    98

--- CodeTop/98.isValidBST.cpp
+++ CodeTop/98.isValidBST.cpp
@@ -16,18 +16,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        
+        return isBST(root,LONG_MIN,LONG_MAX);
+    }
+
+    bool isBST(TreeNode* root, long lower, long upper){
+        if(root == nullptr){
+            return true;
+        }
+        if(root->val <= lower || root->val >= upper){
+            return false;
+        }
+        return isBST(root->left,lower,root->val) && isBST(root->right,root->val,upper);
     }
 };
 // @lc code=end
